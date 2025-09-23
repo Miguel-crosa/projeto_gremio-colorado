96df9b2e3eff55ce535385ed921633de8f9b3fb1
Miguel CRosa <miguel.c.rosa6@aluno.senai.br>
2025-09-23 17:18:43 -0300

    Atualizacoes + sucesso.html

--- _js/cadastro.js
+++ _js/cadastro.js
@@ -1,23 +1,56 @@
+var modal = document.querySelector(".json-informacoes");
+
 function validarDados() {
     const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
     const telefone = document.getElementById('telefone').value;
-    const idade = document.getElementById('idade').value;
-    const turma = document.getElementById('turma').value;
-    const esporte = document.getElementById('esporte').value;
 
+    const nomeRegex = /^[A-Za-zÀ-ú \'-]+$/;
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const telRegex = /\(\d{2}\)\s?\d{4,5}-\d{4}/; 
+    const telRegex = /\(\d{2}\)\s\d{4,5}\-\d{4}/;
 
+    if (!nomeRegex.test(nome)) {
+        alert('Digite seu nome corretamente!');
+        return false;
+    }
     if (!emailRegex.test(email)) {
-        console.log('Erro ao colocar o email');
+        alert('Erro ao colocar o email');
         return false;
     }
 
-    if (!telRegex.test()) {
-        console.log('Erro ao colocar o telefone');
+    if (!telRegex.test(telefone)) {
+        alert('Erro ao colocar o telefone');
         return false;
     }
-    
+
+    window.location.href = "./index.html";
+    return true;
+
 }
 
+function converterJson() {
+
+    modal.style.display = 'flex';
+
+    const dadosJson = {};
+
+    dadosJson.nome = document.querySelector('#nome').value;
+    dadosJson.email = document.querySelector('#email').value;
+    dadosJson.telefone = document.querySelector('#telefone').value;
+    dadosJson.idade = document.querySelector('#idade').value;
+    dadosJson.turma = document.querySelector('#turma').value;
+    dadosJson.esporte = document.querySelector('.esporte').value;
+    dadosJson.novidade = document.querySelector('#radio-escolha').value;
+
+    const json = JSON.stringify(dadosJson);
+
+    document.getElementById("texto-div").innerHTML = json;
+
+
+}
+
+document.getElementById("fechar").addEventListener('click', function () {
+
+    modal.style.display = 'none';
+
+});
